3a974f80ee1cb12410f407676ccf41e7a6af20f3
Erica233 <64255604+Erica233@users.noreply.github.com>
2021-11-05 11:39:17 +0800

    Add files via upload

--- P3_arbitrage/project3.py
+++ P3_arbitrage/project3.py
@@ -16,62 +16,70 @@ from p3tests import *
 
 """
 detectArbitrage
+
+This function will use Bellman-Ford algorithm to detect cycle, 
+if there exists a cycle, it means there is an opportunity of arbitrage.
+If there exists a cycle, it will return a list of cycle path,
+otherwise it will return an empty list.
+
+INPUTS
+adjList: The adjacency list for the currencies.
+adjMat: The adjacency matrix of edge weights for the graph.
+tol: tolerance value (default = 1e-15)
+
+OUTPUTS
+returns the detected cycle path
 """
 def detectArbitrage(adjList, adjMat, tol=1e-15):
     ##### Your implementation goes here. #####
-    # initialize
+    # initialize dist and prev for each vertex
     for vertex in adjList:
         vertex.dist = math.inf
         vertex.prev = None
+
+    # choose a start vertex, and set its dist to zero
     start_vertex = adjList[0]
     start_vertex.dist = 0
 
-    #Bellman-Ford
+    # perform Bellman-Ford (iterate |v|-1 times)
     for iter in range(0, len(adjList) - 1):
-        #change = 0
         for u in adjList:
-            for neighbor in u.neigh:
-                if neighbor.dist > u.dist + adjMat[u.rank][neighbor.rank] + tol:
-                    neighbor.dist = u.dist + adjMat[u.rank][neighbor.rank]
-                    neighbor.prev = u
-                    #change = 1
-    #detect cycle
+            for neigh in u.neigh:
+                if neigh.dist > u.dist + adjMat[u.rank][neigh.rank] + tol:
+                    neigh.dist = u.dist + adjMat[u.rank][neigh.rank]
+                    neigh.prev = u
+
+    # perform an extra iteration to detect cycle, and pick a vertex in a cycle
     vertex_in_cycle = None
     for u in adjList:
-        for neighbor in u.neigh:
-            if neighbor.dist > u.dist + adjMat[u.rank][neighbor.rank] + tol:
-                print("detect")
-                vertex_in_cycle = neighbor
+        for neigh in u.neigh:
+            # if a dist of a vertex changes, a cycle is detected
+            if neigh.dist > u.dist + adjMat[u.rank][neigh.rank] + tol:
+                vertex_in_cycle = neigh
                 break
         if vertex_in_cycle:
-            print("break")
             break
 
-    #find the cycle
+    # find a cycle path backwards
     reverse_cycle = []
     if vertex_in_cycle:
         reverse_cycle += [vertex_in_cycle.rank]
         curr_vertex = vertex_in_cycle.prev
         while curr_vertex:
+            # if find the start of a cycle
             if curr_vertex.rank in reverse_cycle:
                 reverse_cycle += [curr_vertex.rank]
-                print(reverse_cycle)
                 break
             reverse_cycle += [curr_vertex.rank]
             curr_vertex = curr_vertex.prev
-            print(reverse_cycle)
 
-    #remove vertices not in the cycle, and reverse back
+    # remove vertices not in the cycle, and reverse back
     cycle_path = []
-    print(reverse_cycle[::-1])
     for rank in reverse_cycle[::-1]:
         if rank in cycle_path:
             cycle_path.append(rank)
-            print("cycle path", cycle_path)
             break
         cycle_path.append(rank)
-        print("cycle path", cycle_path)
-
 
     return cycle_path
     ##### Your implementation goes here. #####
@@ -80,19 +88,21 @@ def detectArbitrage(adjList, adjMat, tol=1e-15):
 
 """
 rates2mat
+
+This function will transfer the matrix of exchange rates into edge weights, 
+which are -log(exchange rate).
+
+INPUTS
+rates: the matrix of exchange rates
+
+OUTPUTS
+return a matrix of edge weights
 """
 def rates2mat(rates):
     ##### Your implementation goes here. #####
     # Currently this only returns a copy of the rates matrix.
-    #return [[R for R in row] for row in rates]
-    list = [[R for R in row] for row in rates]
-    ##### Your implementation goes here. #####
-    #list = [[-math.log(R) for R in row] for row in rates]
-    #print("rates2mat")
-    print(list)
-    #print("rates2mat end")
     return [[-math.log(R) for R in row] for row in rates]
-    #return list
+    ##### Your implementation goes here. #####
 
 """
 Main function.
