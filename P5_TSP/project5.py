ac3f2ce790597cff1bb8a7069a48abb2fa0342ef
Erica233 <64255604+Erica233@users.noreply.github.com>
2021-12-04 20:33:51 -0500

    comments

--- P5_TSP/project5.py
+++ P5_TSP/project5.py
@@ -20,14 +20,25 @@ from p5priorityQueue import *
 
 """
 Prim's Algorithm
+
+This function will implement Prim's Algorithm, which is used to get 
+the minimal spanning tree by finding the lightest cost edge 
+leading out of the current tree then adding the edge to the tree.
+
+INPUTS
+adjList: a adjacency list of vertices.
+adjMat: a adjacency matrix storing edge weights.
+
+OUTPUTS
+there is no return, it directly modifies the vertices' attributes
 """
 def prim(adjList, adjMat):
-    ##### Your implementation goes here. #####
     # initialize vertices
     for vertex in adjList:
         vertex.cost = math.inf
         vertex.prev = None
         vertex.visited = False
+
     # select and set the start vertex
     start = adjList[0]
     start.cost = 0
@@ -36,15 +47,17 @@ def prim(adjList, adjMat):
     pqueue = PriorityQueue(adjList)
 
     while not pqueue.isEmpty():
+        # get the next vertex
         vertex = pqueue.deleteMin()
         vertex.visited = True
+
         for neighbor in vertex.neigh:
+            # if the edge leads out, update
             if not neighbor.visited:
                 if neighbor.cost > adjMat[vertex.rank][neighbor.rank]:
                     neighbor.cost = adjMat[vertex.rank][neighbor.rank]
                     neighbor.prev = vertex
 
-    return
 
 ################################################################################
 
@@ -52,18 +65,33 @@ def prim(adjList, adjMat):
 Kruskal's Algorithm
 Note: the edgeList is ALREADY SORTED!
 Note: Use the isEqual method of the Vertex class when comparing vertices.
+
+This function will implement Kruskal's Algorithm, which is used to get 
+the minimal spanning tree by repeatedly adding the mininum cost edge 
+that will not form a cycle, until there are no more edges can be added.
+
+INPUTS
+adjList: a adjacency list of vertices.
+edgeList: a list of edges objects.
+
+OUTPUTS
+returns the MST, which is a list of edge objects
 """
 def kruskal(adjList, edgeList):
-    ##### Your implementation goes here. #####
+    # initialize all singleton sets for each vertex
     for vertex in adjList:
         makeset(vertex)
-    X = []
+
+    mst = []
     for edge in edgeList:
+
+        # if the min edge crosses a cut, add it to MST
         u, v = edge.vertices
         if not find(u).isEqual(find(v)):
-            X.append(edge)
+            mst.append(edge)
             union(u, v)
-    return X
+
+    return mst
 
 ################################################################################
 
@@ -77,40 +105,63 @@ These functions will operate directly on the input vertex objects.
 """
 
 """
-makeset: this function will create a singleton set with root v.
+makeset
+
+This function will create a singleton set with root v.
+
+INPUTS
+v: a vertex
+
+OUTPUTS
+there is no return
 """
 def makeset(v):
-    ##### Your implementation goes here. #####
     v.pi = v
     v.height = 0
-    return
+
 
 """
-find: this function will return the root of the set that contains v.
+find
+This function will return the root of the set that contains v.
 Note: we will use path compression here.
 
+INPUTS
+v: a vertex
+
+OUTPUTS
+return the root vertex of the set that contains v
 """
 def find(v):
-    ##### Your implementation goes here. #####
     if not v.isEqual(v.pi):
         v.pi = find(v.pi)
     return v.pi
 
 """
-union: this function will union the sets of vertices v and u.
+union
+
+This function will union the sets of vertices v and u.
+
+INPUTS
+u: a vertex
+v: a vertex
+
+OUTPUTS
+there is no return
 """
 def union(u,v):
-    ##### Your implementation goes here. #####
+    # find the roots of the trees for u and v
     ru = find(u)
     rv = find(v)
 
     if ru == rv:
         return
 
+    # make shorter set point to taller set
     if ru.height > rv.height:
         rv.pi = ru
     elif ru.height < rv.height:
         ru.pi = rv
+    # if both set have same height, arbitrarily break the tie
     else:
         ru.pi = rv
         rv.height += 1
@@ -120,24 +171,41 @@ def union(u,v):
 
 """
 TSP
+
+This function will use depth first search to go through the MST 
+to give an approximation solution to traveling salesman problem.
+
+INPUTS
+adjList: a adjacency list of vertices.
+start: the start vertex
+
+OUTPUTS
+returns a list of vertex ranks representing the cycle tour sequence
 """
 def tsp(adjList, start):
-    ##### Your implementation goes here. #####
+    # initialize the vertices
     for vertex in adjList:
         vertex.visited = False
 
     tour = []
     stack = []
 
+    # visit the start vertex
     start.visited = True
     stack.append(start)
+
     while len(stack) != 0:
+        # get the next vertex
         curr = stack.pop()
         tour.append(curr.rank)
+
+        # visit its unvisited neighbors
         for neighbor in curr.mstN:
             if not neighbor.visited:
                 neighbor.visited = True
                 stack.append(neighbor)
+                
+    # return back to the start vertex
     tour.append(start.rank)
 
     return tour
